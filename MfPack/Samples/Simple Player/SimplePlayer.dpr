e204038a4c56b7888d632696cdeddff0deab1748
FactoryX <41634066+FactoryXCode@users.noreply.github.com>
2020-08-29 22:49:15 +0200

    Added Sample Simple Player

---
+++ MfPack/Samples/Simple Player/SimplePlayer.dpr
@@ -0,0 +1,19 @@
+program SimplePlayer;
+
+uses
+  madExcept,
+  madLinkDisAsm,
+  madListHardware,
+  madListProcesses,
+  madListModules,
+  Vcl.Forms,
+  frmSimplePlayer in 'frmSimplePlayer.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
