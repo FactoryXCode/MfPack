f92367627ab45dcd8ef82398bf84e8ec00002e28
FactoryX <41634066+FactoryXCode@users.noreply.github.com>
2021-11-12 17:09:56 +0100

    Update WinApi.JoyStickApi.pas

--- MfPack/src/WinApi.JoyStickApi.pas
+++ MfPack/src/WinApi.JoyStickApi.pas
@@ -10,7 +10,7 @@
 // Release date: 17-05-2020
 // Language: ENU
 //
-// Revision Version: 3.0.1
+// Revision Version: 3.1.0
 // Description: ApiSet Contract for api-ms-win-mm-joystick-l1-1-0
 //
 // Organisation: FactoryX
@@ -21,17 +21,17 @@
 // CHANGE LOG
 // Date       Person              Reason
 // ---------- ------------------- ----------------------------------------------
-// 13/08/2020 All                 Enigma release. New layout and namespaces
+// 28/10/2021 All                 Bowie release  SDK 10.0.22000.0 (Windows 11)
 //------------------------------------------------------------------------------
 //
 // Remarks: -
 //
 // Related objects: -
-// Related projects: MfPackX301
+// Related projects: MfPackX310
 // Known Issues: -
 //
-// Compiler version: 23 up to 33
-// SDK version: 10.0.19041.0
+// Compiler version: 23 up to 34
+// SDK version: 10.0.22000.0
 //
 // Todo: -
 //
@@ -61,6 +61,7 @@ unit WinApi.JoyStickApi;
 interface
 
 uses
+  {WinApi}
   WinApi.Windows,
   WinApi.MMSysCom;
 
